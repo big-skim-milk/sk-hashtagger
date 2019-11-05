bee62bce1f813d5fb04064ff94fd70bfc773abec
big-skim-milk <57117933+big-skim-milk@users.noreply.github.com>
2019-11-04 20:42:54 -0500

    Update sk-tagger.js

--- sk-tagger.js
+++ sk-tagger.js
@@ -1,33 +1,47 @@
 export function tagger(element, options, callback) {
- if (element.type !== "$w.Text") console.error("SK_Tagger:" + element.id + " is not a Text element.");
- else {
-  setTimeout(function () {
-   let sum = 0,
-    str = '',
-    tags = [],
-    data = options.tags.split(options.delimiter).map(d => { return { k: function () { return Math.round(this.t.length * ((options.font_size + options.whitespace) * options.ratio * 0.49)) }, t: `${options.symbol ? '#' : ''}${d}` }; });
-  data.forEach((im, ix, arr) => {
-   str += options.href ? '<a href="' + options.href[ix] + (options.target ? '" target="' + options.target + '"': '') + `>${im.t}</a>${'&nbsp;'.repeat(options.whitespace)}` : `${im.t}${'&nbsp;'.repeat(options.whitespace)}`;
-   tags.push({ ln: im.k() + options.font_size });
-   if (ix === data.length - 1) {
-     tags.forEach((it, ind) => {
-       sum += it.ln;
-       it.loc = sum;
-       if (ind === tags.length - 1) {
-        element.html = element.html.replace(element.html.replace(/<[^>]*>?/gm, ""), `<span style=${"font-size:" + options.font_size + "px;" + options.formatting}>${str}</span>`);
-        element.onClick(ev => {
-         if (callback) {
-           let j = 0;
-           tags.forEach((i, ii, iii) => {
-             j += ev.offsetX > i.loc ? 1 : 0;
-             if (ii === iii.length - 1 && data[j]) callback(data[j].t);
-           });
-         }
+    let el = () => {
+        if (!Array.isArray(element)) {
+            if (typeof element === 'string') return !element.includes('#') ? $w(`#${element}`) : $w(`${element}`);
+            else if (typeof element === 'object') return element;
+        } else {
+            return element.map(f => {
+                if (typeof f === 'string') return !f.includes('#') ? $w(`#${f}`) : $w(`${f}`);
+                else if (typeof f === 'object') return f;
+            });
+        }
+    };
+    if ((!Array.isArray(el()) && el().type !== '$w.Text') || (Array.isArray(el()) && el()[0].type !== '$w.Text')) console.error('sk-tagger: ' + el().id + ' is not a text element.');
+    if (!options.delimiter) options.delimiter = ', ';
+    options.tags = Array.isArray(options.tags) ? options.tags.join(options.delimiter) : options.tags;
+    if (options.href) options.href = Array.isArray(options.href) ? options.href : options.href.split(options.delimiter);
+    setTimeout(() => !Array.isArray(el()) ? tak(el()) : el().forEach(e => tak(e)), 1);
+
+    function tak(txt) {
+        let sum = 0,
+            str = '',
+            tags = [],
+            data = options.tags.split(options.delimiter).map(d => { return { k: function() { return Math.round(this.t.length * ((options.font_size + options.whitespace) * options.ratio * 0.49)) }, t: `${options.symbol ? '#' : ''}${d}` }; });
+        data.forEach((im, ix) => {
+            str += options.href ? `${options.format_tag ? '<span style=' + options.format_tag + '>' : ''}<a href=${options.href[ix]} ${options.target ? 'target=' + options.target : ''}>${im.t}</a>${options.format_tag ? '</span>' : ''}${'&nbsp;'.repeat(options.whitespace)}` : `${options.format_tag ? '<span style=' + options.format_tag + '>' : ''}${im.t}${options.format_tag ? '</span>' : ''}${'&nbsp;'.repeat(options.whitespace)}`;
+            tags.push({ ln: im.k() + options.font_size });
+            if (ix === data.length - 1) {
+                tags.forEach((it, ind) => {
+                    sum += it.ln;
+                    it.loc = sum;
+                    if (ind === tags.length - 1) {
+                        txt.html = txt.html.replace(txt.html.replace(/<[^>]*>?/gm, ""), `<span style=${"font-size:" + options.font_size + "px;" + options.format_all}>${str}</span>`);
+                        txt.onClick(ev => {
+                            if (callback) {
+                                let j = 0;
+                                tags.forEach((i, ii) => {
+                                    j += ev.offsetX > i.loc ? 1 : 0;
+                                    if (ii === tags.length - 1 && data[j]) callback(data[j].t);
+                                });
+                            }
+                        });
+                    }
+                });
+            }
         });
-       }
-      });
-     }
-    });
-   }, 1);
-  }
+    }
 }
